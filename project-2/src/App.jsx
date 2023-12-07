c8d5b4f9b5a5d7801e96b7cb21e9cebb7980d2f7
UmamaMasroor <umamafatima319@gmail.com>
2023-12-07 17:23:49 +0500

    weather app done

---
+++ project-2/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+ import Weather from './Components/Weather App/Weather'
+
+function App() {
+ 
+
+  return (
+    <>
+    
+      <Weather/>
+    </>
+  )
+}
+
+export default App;
